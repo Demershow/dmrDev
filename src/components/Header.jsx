2f69e3741e4a801f8cbec6d795e9c1e634142a27
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2022-10-21 17:15:16 -0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/Header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+    <ul className="itensHeader">
+      <li>About</li>
+      <li>Home</li>
+      <li>Contact</li>
+    </ul>
+  </div>
+  );
+}
